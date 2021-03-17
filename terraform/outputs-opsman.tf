b855ce08735fce0320d0c688e9d4cee87df7f6ca
drawsmcgraw <drew.malone@gmail.com>
2021-03-17 17:19:31 -0400

    initial commit

---
+++ terraform/outputs-opsman.tf
@@ -0,0 +1,69 @@
+locals {
+  stable_config_tas_importer = {
+    access_key         = var.access_key
+    #secret_key         = var.secret_key
+    environment_name   = var.environment_name
+    availability_zones = var.availability_zones
+    region             = var.region
+
+    vpc_id = aws_vpc.vpc.id
+
+    public_subnet_ids   = aws_subnet.public-subnet[*].id
+    public_subnet_cidrs = aws_subnet.public-subnet[*].cidr_block
+
+    #management_subnet_ids   = aws_subnet.management-subnet[*].id
+    #management_subnet_cidrs = aws_subnet.management-subnet[*].cidr_block
+    #management_subnet_gateways = [
+    #  for i in range(length(var.availability_zones)) :
+    #  cidrhost(aws_subnet.management-subnet[i].cidr_block, 1)
+    #]
+    #management_subnet_reserved_ip_ranges = [
+    #  for i in range(length(var.availability_zones)) :
+    #  "${cidrhost(aws_subnet.management-subnet[i].cidr_block, 1)}-${cidrhost(aws_subnet.management-subnet[i].cidr_block, 9)}"
+    #]
+
+    #tas_importer_subnet_id                 = aws_subnet.public-subnet[0].id
+    #tas_importer_public_ip                 = aws_eip.tas-importer.public_ip
+    tas_importer_public_ip                 =  aws_instance.tas-importer.public_ip
+    #tas_importer_dns                       = aws_route53_record.tas-importer.name
+    #tas_importer_iam_user_access_key       = aws_iam_access_key.tas-importer.id
+    #tas_importer_iam_user_secret_key       = aws_iam_access_key.tas-importer.secret
+    tas_importer_iam_instance_profile_name = aws_iam_instance_profile.tas-importer.name
+    #tas_importer_key_pair_name             = aws_key_pair.tas-importer.key_name
+    tas_importer_key_pair_name             = aws_instance.tas-importer.key_name
+    #tas_importer_ssh_public_key            = tls_private_key.tas-importer.public_key_openssh
+    #tas_importer_ssh_private_key           = tls_private_key.tas-importer.private_key_pem
+    #tas_importer_bucket                    = aws_s3_bucket.tas-importer-bucket.bucket
+    tas_importer_security_group_id         = aws_security_group.tas-importer.id
+    tas_importer_security_group_name       = aws_security_group.tas-importer.name
+
+    ###platform_vms_security_group_id   = aws_security_group.platform.id
+    ###platform_vms_security_group_name = aws_security_group.platform.name
+
+    #tkgi_security_group_id   = aws_security_group.tkgi-umbrella.id
+    #tkgi_security_group_name = aws_security_group.tkgi-umbrella.name
+
+    #nat_security_group_id   = aws_security_group.nat.id
+    #nat_security_group_name = aws_security_group.nat.name
+
+    #services_subnet_ids   = aws_subnet.services-subnet[*].id
+    #services_subnet_cidrs = aws_subnet.services-subnet[*].cidr_block
+    #services_subnet_gateways = [
+    #  for i in range(length(var.availability_zones)) :
+    #  cidrhost(aws_subnet.services-subnet[i].cidr_block, 1)
+    #]
+    #services_subnet_reserved_ip_ranges = [
+    #  for i in range(length(var.availability_zones)) :
+    #  "${cidrhost(aws_subnet.services-subnet[i].cidr_block, 1)}-${cidrhost(aws_subnet.services-subnet[i].cidr_block, 9)}"
+    #]
+
+    #ssl_certificate = var.ssl_certificate
+    #ssl_private_key = var.ssl_private_key
+  }
+}
+
+output "stable_config_opsmanager" {
+  value     = jsonencode(local.stable_config_tas_importer)
+  sensitive = true
+}
+
---
+++ terraform/tas-importer.tf
@@ -0,0 +1,170 @@
+#
+# Jump Box
+#
+
+data "aws_ami" "amazon_linux" {
+  # Latest Amazon Linux 2
+  most_recent = true
+  name_regex = "^amzn2-ami-hvm-[0-9.]+-x86_64-ebs$"
+  owners = ["amazon"]
+}
+
+###resource "aws_instance" "jump_box" {
+###  ami                         = data.aws_ami.amazon_linux.id
+###  instance_type               = "m4.large"
+###  key_name                    = var.opsman_keypair
+###  vpc_security_group_ids      = [aws_security_group.tas-importer.id]
+###  subnet_id                   = aws_subnet.public-subnet[0].id
+###
+###  root_block_device {
+###    volume_type = "gp2"
+###    volume_size = 50
+###  }
+###
+###  tags = {
+###    "Name" = "jump-box"
+###  }
+###
+###}
+
+
+#
+# Ops Manager VM plus requisite IAM resources
+#
+
+resource "aws_instance" "tas-importer" {
+  ami                         = data.aws_ami.amazon_linux.id
+  ###ami = var.opsman_ami
+  instance_type = "t2.large"
+  key_name = var.opsman_keypair
+  vpc_security_group_ids = [aws_security_group.tas-importer.id]
+  subnet_id = aws_subnet.public-subnet[0].id
+  iam_instance_profile = aws_iam_instance_profile.tas-importer.id
+  root_block_device {
+    volume_size = 150
+  }
+
+  tags = {
+    Name = "${var.environment_name}-opsman"
+  }
+}
+
+resource "aws_iam_policy" "tas-importer-role" {
+  name   = "${var.environment_name}-tas-importer-role"
+  policy = data.aws_iam_policy_document.tas-importer.json
+}
+
+resource "aws_iam_role_policy_attachment" "tas-importer-policy" {
+  role       = aws_iam_role.tas-importer.name
+  policy_arn = aws_iam_policy.tas-importer-role.arn
+}
+
+resource "aws_iam_role" "tas-importer" {
+  name = "${var.environment_name}-tas-importer-role"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "ec2.amazonaws.com"
+        ]
+      },
+      "Action": [
+        "sts:AssumeRole"
+      ]
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_instance_profile" "tas-importer" {
+  name = "${var.environment_name}-tas-importer"
+  role = aws_iam_role.tas-importer.name
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+}
+
+data "aws_iam_policy_document" "tas-importer" {
+  statement {
+    sid       = "OpsMgrInfoAboutCurrentInstanceProfile"
+    effect    = "Allow"
+    actions   = ["iam:GetInstanceProfile"]
+    resources = [aws_iam_instance_profile.tas-importer.arn]
+  }
+
+  statement {
+    sid     = "OpsMgrCreateInstanceWithCurrentInstanceProfile"
+    effect  = "Allow"
+    actions = ["iam:PassRole"]
+    resources = [
+      aws_iam_role.tas-importer.arn,
+      #aws_iam_role.pks-master.arn,
+      #aws_iam_role.pks-worker.arn,
+    ]
+  }
+
+  statement {
+    sid     = "OpsMgrS3Permissions"
+    effect  = "Allow"
+    actions = ["s3:*"]
+    resources = [
+      "arn:aws:s3:::${var.environment_name}-tas-importer-bucket",
+      "arn:aws:s3:::${var.environment_name}-tas-importer-bucket/*"
+      # Use these values for govcloud
+      #"arn:aws-us-gov:s3:::${var.environment_name}-tas-importer-bucket",
+      #"arn:aws-us-gov:s3:::${var.environment_name}-tas-importer-bucket/*"
+    ]
+  }
+
+  statement {
+    sid    = "OpsMgrEC2Permissions"
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeKeypairs",
+      "ec2:DescribeVpcs",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeImages",
+      "ec2:DeregisterImage",
+      "ec2:DescribeSubnets",
+      "ec2:RunInstances",
+      "ec2:DescribeInstances",
+      "ec2:TerminateInstances",
+      "ec2:RebootInstances",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DescribeTargetHealth",
+      "elasticloadbalancing:RegisterTargets",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+      "ec2:DescribeAddresses",
+      "ec2:DisassociateAddress",
+      "ec2:AssociateAddress",
+      "ec2:CreateTags",
+      "ec2:DescribeVolumes",
+      "ec2:CreateVolume",
+      "ec2:AttachVolume",
+      "ec2:DeleteVolume",
+      "ec2:DetachVolume",
+      "ec2:CreateSnapshot",
+      "ec2:DeleteSnapshot",
+      "ec2:DescribeSnapshots",
+      "ec2:DescribeRegions"
+    ]
+    resources = ["*"]
+  }
+}
+
